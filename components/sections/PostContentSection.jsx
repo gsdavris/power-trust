59bd5d13a572e57b8ce95db25d7ca496700c0645
gsdavris <58606715+gsdavris@users.noreply.github.com>
2024-01-09 15:12:26 +0000

    feat: Remove commented code from PostContentSection.jsx and update label for 'Previous' button in blog index page

--- components/sections/PostContentSection.jsx
+++ components/sections/PostContentSection.jsx
@@ -24,7 +24,7 @@ export default function PostContentSection({
                 {title}
               </h1>
             )}
-            {/* {date && (
+            {date && (
               <p className="mt-6 text-xl leading-8 text-gray-700">
                 <time dateTime={date} className="text-gray-500">
                   {new Date(date).toLocaleDateString("el", {
@@ -34,7 +34,7 @@ export default function PostContentSection({
                   })}
                 </time>
               </p>
-            )} */}
+            )}
           </div>
         </div>
         <div className="-ml-12 -mt-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -222,7 +222,7 @@ export default function BlogPage({ data }) {
             ) : (
               <>
                 <HiChevronLeft className="mr-2 h-5 w-5" aria-hidden="true" />
-                Προηγούμενα
+                Προηγούμενα άρθρα
               </>
             )}
           </button>
