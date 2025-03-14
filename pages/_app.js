f9bcd885e5ddc72f68ec6f00d50d6b2ff427c8af
gsdavris <58606715+gsdavris@users.noreply.github.com>
2025-03-14 15:25:02 +0000

    GTAG added

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import { useRouter } from "next/router";
 import AOS from "aos";
 import "../styles/global.scss";
 import "aos/dist/aos.css";
@@ -7,6 +8,27 @@ import { ApolloProvider } from "@apollo/client";
 import client from "../apollo/client";
 
 export default function MyApp({ Component, pageProps, router }) {
+  const myRouter = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      // Push to dataLayer
+      window.dataLayer = window.dataLayer || [];
+      window.dataLayer.push({
+        event: "pageview",
+        page: url,
+      });
+    };
+
+    // When the component is mounted, subscribe to router changes
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    // If the component is unmounted, unsubscribe
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   useEffect(() => {
     AOS.init({
       duration: 800,
